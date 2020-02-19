a4afd48e74b19623fc2f7c2a39e1ad09a8a8102a
Mark Mandel <markmandel@google.com>
2020-02-18 16:23:04 -0800

    Go SDK alpha implementation.

---
+++ sdks/go/alpha.go
@@ -0,0 +1,50 @@
+// Copyright 2020 Google LLC All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sdk
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"agones.dev/agones/pkg/sdk/alpha"
+	"google.golang.org/grpc"
+)
+
+// Alpha is the struct for Alpha functionality
+type Alpha struct {
+	client alpha.SDKClient
+	ctx    context.Context
+}
+
+func newAlpha(conn *grpc.ClientConn) *Alpha {
+	return &Alpha{
+		ctx:    context.Background(),
+		client: alpha.NewSDKClient(conn),
+	}
+}
+
+// Get the last player capacity that was set through the SDK.
+// if the player capacity is set from outside the SDK, use SDK.Alpha.GameServer() instead.
+func (a *Alpha) GetPlayerCapacity() (int64, error) {
+	c, err := a.client.GetPlayerCapacity(a.ctx, &alpha.Empty{})
+	return c.Count, errors.Wrap(err, "could not get player capacity")
+}
+
+// SetPlayerCapacity changes the player capacity to a new value
+func (a *Alpha) SetPlayerCapacity(cap int64) error {
+	_, err := a.client.SetPlayerCapacity(a.ctx, &alpha.Count{Count: cap})
+	return errors.Wrap(err, "could not set player capacity")
+}
